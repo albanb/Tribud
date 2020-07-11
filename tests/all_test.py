98e72a14d4e24718da39d90fa495becc89d5482f
albanb <binmail@free.fr>
2020-07-11 15:20:38 +0200

    Return the tests results

--- tests/all_test.py
+++ tests/all_test.py
@@ -6,10 +6,12 @@ Gather all tests for tribud.
 # pylint: disable=missing-function-docstring
 # No need to add a docstring to each tests.
 
+import sys
 import unittest
 import config_test
 
 if __name__ == '__main__':
     suite1 = config_test.suite_config_test()
     all_tests = unittest.TestSuite([suite1])
-    unittest.TextTestRunner(verbosity=2).run(all_tests)
+    result = unittest.TextTestRunner(verbosity=2).run(all_tests)
+    sys.exit(not result.wasSuccessful())
